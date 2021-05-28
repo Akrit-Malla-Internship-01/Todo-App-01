0753505750fffcc1885385457e6e6723edfe5542
Akrit08 <newar.akrit08@gmail.com>
2021-05-28 17:42:08 -0300

    Add some style, and space in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default class classComponent extends Component {
       <div>
         <header className="main-container__title main-container__title--color main-container__title--size">
           <i className="fas fa-check-square"></i>
-          <h2>My Todos </h2>
+          <h2>&nbsp; My Todos</h2>
         </header>
       </div>
     );
